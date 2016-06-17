67aac1288b7f250d7aa9dcae177f9033f55a24b6
Iain Stewart <iain.stewart08@gmail.com>
2016-06-17 11:15:02 +0100

    initial room tests

--- karaoke_bar/room.rb
+++ karaoke_bar/room.rb
@@ -1,4 +1,9 @@
 class Room
+  attr_reader :name, :price, :songs
 
-
+  def initialize(params)
+     @name = params[:name]
+     @price = params[:price]
+     @songs = []
+   end
 end
--- karaoke_bar/specs/room_spec.rb
+++ karaoke_bar/specs/room_spec.rb
@@ -2,5 +2,19 @@ require('minitest/autorun')
 require_relative('../room')
 
 class TestRoom < MiniTest::Test
+  def setup
+    @room = Room.new(name: "Pop", price: 20)
+  end
 
+  def test_room_has_name
+    assert_equal("Pop", @room.name)
+  end
+
+  def test_room_has_price
+    assert_equal(20, @room.price)
+  end
+
+  def test_room_has_empty_song_array
+    assert_equal([], @room.songs)
+  end
 end
