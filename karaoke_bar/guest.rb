812083799af9bea83ae9944c7b412311dfcff379
Iain Stewart <iain.stewart08@gmail.com>
2016-06-17 10:58:39 +0100

    first name test

--- karaoke_bar/guest.rb
+++ karaoke_bar/guest.rb
@@ -1,2 +1,8 @@
 class Guest
+
+  attr_reader :first_name
+  
+  def initialize(params)
+     @first_name = params[:first_name]
+   end
 end
--- karaoke_bar/specs/guest_spec.rb
+++ karaoke_bar/specs/guest_spec.rb
@@ -2,7 +2,12 @@ require('minitest/autorun')
 require_relative('../guest')
 
 class TestGuest < MiniTest::Test
-  def test___
-    
+
+  def setup
+    @guest = Guest.new(first_name: "John", last_name: "Smith", age: 26)
+  end
+
+  def test_guest_has_first_name
+    assert_equal("John", @guest.first_name)
   end
 end
