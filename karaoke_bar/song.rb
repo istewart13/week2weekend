128e20aa364a5445eb6505398ff3c0a8dbd3b6e6
Iain Stewart <iain.stewart08@gmail.com>
2016-06-17 11:05:57 +0100

    song tests

--- karaoke_bar/song.rb
+++ karaoke_bar/song.rb
@@ -1,2 +1,10 @@
 class Song
+
+  attr_reader :title, :artist, :genre
+
+  def initialize(params)
+     @title = params[:title]
+     @artist = params[:artist]
+     @genre = params[:genre]
+   end
 end
--- karaoke_bar/specs/song_spec.rb
+++ karaoke_bar/specs/song_spec.rb
@@ -2,7 +2,19 @@ require('minitest/autorun')
 require_relative('../song')
 
 class TestSong < MiniTest::Test
-  def test___
-    
+  def setup
+    @song = Song.new(title: "Like A Prayer", artist: "Madonna", genre: "Pop")
+  end
+
+  def test_guest_has_title
+    assert_equal("Like A Prayer", @song.title)
+  end
+
+  def test_guest_has_artist
+    assert_equal("Madonna", @song.artist)
+  end
+
+  def test_guest_has_genre
+    assert_equal("Pop", @song.genre)
   end
 end
